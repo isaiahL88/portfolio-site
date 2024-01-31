08af41c177ee75dc46960fa8edda81f67be8a587
isaiahL88 <isaiah.ib@outlook.com>
2024-01-31 15:52:33 -0500

    added lift app demo

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -63,12 +63,16 @@ const Projects = forwardRef((props, ref) => {
                     options={mobileView ? optionsMobile : options}
                     aria-label='lift'>
                     <SplideSlide>
+                        <iframe width="400" height="600" src="https://www.youtube.com/embed/iLUuC1vdpBA?si=-X6g2qlepT6qTNpz" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+                    </SplideSlide>
+                    <SplideSlide>
                         <img className='lift-img' src={lift} />
 
                     </SplideSlide>
                     <SplideSlide>
                         <img className='lift-img' src={lift2} />
                     </SplideSlide>
+
                 </Splide>
             </div>
             <div className='project'>
