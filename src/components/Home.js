a8eebb0dc72a8973a46ad43f15c90103bb81b258
isaiahL88 <isaiah.ib@outlook.com>
2024-10-21 03:13:11 -0400

    lottie fix

--- src/components/Home.js
+++ src/components/Home.js
@@ -46,7 +46,6 @@ import covidImg from "../assets/covid.png"
 import framerImg from "../assets/Framer-Motion.webp"
 
 import { saveAs } from 'file-saver';
-import Lottie, { LottieRefCurrentProps } from "lottie-react"
 import animationData from '../assets/darklight.json'
 
 const Home = () => {
